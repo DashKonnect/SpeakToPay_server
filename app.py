fe5846e4cecad6de2c5c8ab93ecfec42be300058
lezwon <lezwon@gmail.com>
2017-11-03 19:21:07 +0530

    added api support

--- app.py
+++ app.py
@@ -1,26 +1,28 @@
 from flask import Flask
+from flask import request
 from google.cloud import translate
+import json
 import os
 
 app = Flask(__name__)
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./SpeakToPay-696d8f6e86f0.json"
+translate_client = translate.Client()
+
 @app.route('/')
 def homepage():
-    # Instantiates a client
-    translate_client = translate.Client()
+	text = u'Hello, world!'
+	target = 'ru'
+	translation = translate_client.translate(text, target_language=target)
+	return u'Text: {}'.format(text) + u'Translation: {}'.format(translation['translatedText'])
 
-    # The text to translate
-    text = u'Hello, world!'
-    # The target language
-    target = 'ru'
+@app.route('/translate', methods=['POST'])
+def translate():
+	target = 'en'
 
-    # Translates some text into Russian
-    translation = translate_client.translate(
-        text,
-        target_language=target)
-
-    return u'Text: {}'.format(text) + u'Translation: {}'.format(translation['translatedText'])
+	raw_message = request.data.decode(encoding='utf-8')
+	translation = translate_client.translate(raw_message,target_language=target)
+	return json.dumps(translation)
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
+	app.run(debug=True, use_reloader=True)
 
