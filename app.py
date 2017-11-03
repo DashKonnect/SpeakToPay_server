8a7857f7f9678d37366794f5d48e96c3079a2a6e
Jaison <jaisonsaji@gmail.com>
2017-11-03 23:31:28 +0530

    return string

--- app.py
+++ app.py
@@ -12,13 +12,15 @@ def homepage():
 	translation = translate_client.translate(text, target_language=target)
 	return u'Text: {}'.format(text) + u'Translation: {}'.format(translation['translatedText'])
 
-@app.route('/translater', methods=['POST'])
-def translater():
+@app.route('/translate', methods=['POST'])
+def translate():
 	target = 'en'
+	final = []
 	raw_message = request.data.decode(encoding='utf-8')
-	return dialog(raw_message)
+	byte_data = dialog(raw_message)
+	string_data = byte_data.decode(encoding='utf-8')
+	return string_data
 	
-
 if __name__ == '__main__':
 	app.run(debug=True, use_reloader=True)
 
